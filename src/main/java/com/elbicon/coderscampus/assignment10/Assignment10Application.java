46fa6423a1763f3f414a11924abe21fca7276c10
Leslie Fleming <leslie_fleming@elbicon.com>
2021-11-21 18:01:57 +0000

    v1.0.0

--- src/main/java/com/elbicon/coderscampus/assignment10/Assignment10Application.java
+++ src/main/java/com/elbicon/coderscampus/assignment10/Assignment10Application.java
@@ -1,6 +1,8 @@
 package com.elbicon.coderscampus.assignment10;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -8,6 +10,7 @@ public class Assignment10Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Assignment10Application.class, args);
+
     }
 
 }
---
+++ src/main/java/com/elbicon/coderscampus/assignment10/interfaces/Meals.java
@@ -0,0 +1,8 @@
+package com.elbicon.coderscampus.assignment10.interfaces;
+
+import org.springframework.http.ResponseEntity;
+
+public interface Meals<T> {
+    ResponseEntity<T> getWeekMeals(String numCalories, String diet, String exclusions);
+    ResponseEntity<T> getDayMeals(String numCalories, String dient, String exclusions);
+}
